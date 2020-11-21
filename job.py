ee3164bcab3e245da087cdeb4a21a081611e3851
piedude1717 <abhibaisthakur@gmail.com>
2020-11-20 17:28:13 -0800

    hi

--- job.py
+++ job.py
@@ -4,4 +4,18 @@
 
 class Job:
 
-    pass
+    def __init__(self, priority=None, message=None):
+        self.priority = priority
+        self.message = message
+
+    def __eq__(self, other):
+        return self.priority == other.priority
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+    def __gt__(self, other):
+        return self.priority > other.priority
+
+    def __le__(self, other):
+        return self.priority <= other.priority
