5de947e394eed4ec83809194ee3d0276e3a49878
piedude1717 <abhibaisthakur@gmail.com>
2020-11-21 16:45:50 -0800

    finished shitty queue

--- naive_priority_queue.py
+++ naive_priority_queue.py
@@ -1,15 +1,26 @@
 # NaivePriorityQueue (aka 'ShittyQueue'): A simple but inefficient priority queue.
 # Your implementation should pass the tests in test_naive_priority_queue.py.
-# YOUR NAME
+# abhimanyu, bais
 
 class NaivePriorityQueue:
 
-    def __init__(self, data=None):
+    def __init__(self):
         self.data = []
-        self.data.append(data)
 
     def enqueue(self, item):
         return self.data.append(item)
 
     def dequeue(self):
-        return self.data.pop()
+        if self.is_empty():
+            return None
+        highest_priority = 0
+        victim = 0
+        while victim < len(self.data):
+            if self.data[highest_priority] < self.data[victim]:
+                highest_priority = victim
+            victim += 1
+        return self.data.pop(highest_priority)
+
+    def is_empty(self):
+        return len(self.data) == 0
+
